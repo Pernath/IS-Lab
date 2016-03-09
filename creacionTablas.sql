343c51b9456631ea07e6a5537b0e035f4390ccc1
Pernath <cgah.95@gmail.com>
2016-03-08 18:57:38 -0600

    Corrección
    Reacomodo del script SQL

--- creacionTablas.sql
+++ creacionTablas.sql
@@ -1,6 +1,20 @@
---Autor: Carlos Gerardo Acosta Hernández
---Descripción: SQL Script para la creación de las tablas de la base de datos
+-- Autor: Carlos Gerardo Acosta Hernández
+-- Descripción: SQL Script para la creación de las tablas de la base de datos
 
+-- TABLAS:
+-- Tabla de telefonos a referenciar por el atributo correspondiente de usuarios
+CREATE TABLE TELEFONO (
+id INTEGER PRIMARY KEY,
+numero VARCHAR(15) UNIQUE NOT NULL 
+);
+
+-- Tabla de tipos de sangre a referenciar por el atributo correspondiente de usuarios
+CREATE TABLE TIPOS (
+id INTEGER PRIMARY KEY,
+tiposangre VARCHAR(3) NOT NULL UNIQUE 
+);
+
+-- Tabla de usuario
 CREATE TABLE USUARIO (
 id INTEGER PRIMARY KEY,
 nombre VARCHAR(64) NOT NULL,
@@ -13,15 +27,6 @@ id_telefono INTEGER REFERENCES TELEFONO (id),
 id_tiposangre INTEGER REFERENCES TIPOS (id) )
 ;
 
-CREATE TABLE TELEFONO (
-id INTEGER PRIMARY KEY,
-numero VARCHAR(15) UNIQUE NOT NULL 
-);
-
-CREATE TABLE TIPOS (
-id INTEGER PRIMARY KEY,
-tiposangre VARCHAR(3) NOT NULL UNIQUE 
-);
 
 
 
